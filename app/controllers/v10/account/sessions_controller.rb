303cbefff9276f2b9bc6fdd9e3f7f5d7b797b222
Ricky <rickycxj@gmail.com>
2016-12-21 10:31:07 +0800

    完成用户手机号和邮箱登录

--- app/controllers/v10/account/sessions_controller.rb
+++ app/controllers/v10/account/sessions_controller.rb
@@ -1,2 +1,42 @@
-class V10::Account::SessionsController < ApplicationController
+LOGIN_TYPES = %w(email mobile vcode).freeze
+
+module V10
+  module Account
+    class SessionsController < ApplicationController
+      include Constants::CommonErrorCode
+
+      def create
+        login_type = params[:type]
+        unless LOGIN_TYPES.include?(login_type)
+          return render_api_error(UNSUPPORTED_TYPE)
+        end
+        send("login_by_#{login_type}", login_params)
+      end
+
+      private
+      def login_by_vcode(user_params)
+        api_result = Services::Account::LoginService.login_by_vcode(user_params[:mobile], user_params[:vcode])
+        render_api_user(api_result)
+      end
+
+      def login_by_email(user_params)
+        api_result = Services::Account::LoginService.login_by_email(user_params[:email], user_params[:password])
+        render_api_user(api_result)
+      end
+
+      def render_api_user(api_result)
+        if api_result.is_failure?
+          render_api_error(api_result.code, api_result.msg)
+        else
+          template = "v10/account/users/base"
+          data = api_result.data
+          render template, locals: {api_result: ApiResult.success, user: data[:user]}
+        end
+      end
+
+      def login_params
+        params.permit(:type, :mobile, :email, :vcode, :password)
+      end
+    end
+  end
 end
---
+++ app/services/account/login_service.rb
@@ -0,0 +1,63 @@
+module Services
+  module Account
+    class LoginService
+      include Constants::HttpErrorCode
+      include Constants::CommonErrorCode
+      include Constants::SignErrorCode
+
+      def self.login_by_vcode(mobile, vcode)
+        if mobile.blank? or vcode.blank?
+          return ApiResult.error_result(MISSING_PARAMETER)
+        end
+
+        #查询用户是否存在
+        user = User.by_mobile(mobile)
+        if user.nil?
+          return ApiResult.error_result(USER_NOT_FOUND)
+        end
+
+        #检查用户输入的验证码是否正确
+
+        #移除验证码
+
+        #登录
+        build_login_result user
+
+      end
+
+      def self.login_by_email(email, password)
+        if email.blank? or password.blank?
+          return ApiResult.error_result(MISSING_PARAMETER)
+        end
+
+        user = User.by_email(email)
+        if user.nil?
+          return ApiResult.error_result(USER_NOT_FOUND)
+        end
+
+        #查询出了这个用户  对比密码
+        password_salt = user.password_salt
+        true_password = ::Digest::MD5.hexdigest(password + password_salt)
+
+        if user.password != true_password or password.blank?
+          return ApiResult.error_result(PASSWORD_NOT_MATCH)
+        end
+
+        #登录
+        build_login_result user
+      end
+
+      private
+      def self.build_login_result(user)
+        user.last_visit = Time.now
+        user.save
+
+        data = {
+            user: user
+        }
+
+        ApiResult.success_with_data(data)
+      end
+    end
+  end
+end
