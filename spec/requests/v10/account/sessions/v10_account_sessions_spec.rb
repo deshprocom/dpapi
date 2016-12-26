69c57ed751400662cb84e5f091b0a9e432841b79
Ricky <rickycxj@gmail.com>
2016-12-26 09:52:02 +0800

    增加app_key校验和user_token后 更改相关的测试用例
    Former-commit-id: fe36986cb0c7b41e9e14f894387d9b60a5041d8a

--- spec/requests/v10/account/sessions/v10_account_sessions_spec.rb
+++ spec/requests/v10/account/sessions/v10_account_sessions_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "/v10/login (SessionsController)", :type => :request do
+  let!(:dpapi_affiliate) { FactoryGirl.create(:affiliate_app) }
+
   let(:http_headers) do
     {
         ACCEPT: "application/json",
-        HTTP_ACCEPT: "application/json"
+        HTTP_ACCEPT: "application/json",
+        HTTP_X_DP_CLIENT_IP: "localhost",
+        HTTP_X_DP_APP_KEY: "467109f4b44be6398c17f6c058dfa7ee"
     }
   end
 
