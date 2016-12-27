5fcfdfb220fe1c0a726b9a4ebd15433d554d9f92
Ricky <rickycxj@gmail.com>
2016-12-27 14:36:10 +0800

    代码调整
    Former-commit-id: 6ffd7e0389b27e07250bcb596b9920a8b90e53ff

--- app/services/account/email_login_service.rb
+++ app/services/account/email_login_service.rb
@@ -22,13 +22,13 @@ module Services
         #判断该用户是否存在
         return ApiResult.error_result(USER_NOT_FOUND) if user.nil?
 
-        #查询出了这个用户  对比密码
+        #查询出了这个用户 对比密码
         salted_passwd = ::Digest::MD5.hexdigest(password + user.password_salt)
         unless salted_passwd.eql?(user.password)
           return ApiResult.error_result(PASSWORD_NOT_MATCH)
         end
 
-        #登录
+        #刷新上次访问时间
         user.touch_visit!
         #生成用户令牌
         app_access_token = AppAccessToken.from_credential(CurrentRequestCredential, user.user_uuid)
