7f0ca45b866040c0545f1ab343697d56b685031e
Ricky <rickycxj@gmail.com>
2016-12-29 17:36:55 +0800

    定义赛事 个人信息相关的路由规则

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,27 @@ Rails.application.routes.draw do
       post 'logout', to: 'sessions#destroy', as: :logout
       post 'register', to: 'accounts#create', as: :register
     end
+
+    namespace :account do
+      resource :forward_password, only: [:create]
+      resource :vcode, only: [:create]
+      resource :verify_vode, only: [:create]
+
+      resources :users, only: [] do
+        resource :profile, only: [:show, :create]
+      end
+    end
+
+    scope module: 'events' do
+      resources :events, param: :node_id, only: [:index, :show]
+      post 'events/:node_id/apply', to: 'apply#create', as: :event_apply
+      post 'events/search', to: 'search#show', as: :event_search
+    end
+
+    scope module: 'users' do
+      resources :users, param: :uuid, only:[:show] do
+        resources :events, only: [:index]
+      end
+    end
   end
 end
