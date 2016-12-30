e13bd8fbd4cfc401ec85ab9bde69add12cdb37f1
Ricky <rickycxj@gmail.com>
2016-12-30 19:51:36 +0800

    代码重构,优化
    Former-commit-id: 69985430227e1f60685c73797db1240ed2d6eff0

--- app/controllers/v10/account/accounts_controller.rb
+++ app/controllers/v10/account/accounts_controller.rb
@@ -30,14 +30,8 @@ module V10
 
       def render_api_result(result)
         return render_api_error(result.code, result.msg) if result.failure?
-
         template = 'v10/account/users/session'
-        app_access_token = result.data.delete(:app_access_token)
-        view_params = {
-          api_result: result,
-          user: result.data[:user],
-          app_access_token: app_access_token
-        }
+        view_params = RenderResultHelper.session_user_result(result)
         render template, locals: view_params
       end
 
