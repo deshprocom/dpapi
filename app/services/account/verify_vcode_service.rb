3619db14123d76abb5d88ab2924d7811497c1a06
Ricky <rickycxj@gmail.com>
2016-12-30 16:30:59 +0800

    修改手机注册需要传密码 增加验证码验证和获取个人信息的接口
    Former-commit-id: 63007c33e29db0dddfac3e5c27a028b5c57640fb [formerly f4d037276c283090b367fc7e310cd1709451bcae]
    Former-commit-id: 7a80c19751474d3e9fe45a5896377bc5cf9e76cc

---
+++ app/services/account/verify_vcode_service.rb
@@ -0,0 +1,26 @@
+module Services
+  module Account
+    class VerifyVcodeService
+      include Serviceable
+      include Constants::Error::Sign
+      attr_accessor :type, :mobile, :vcode
+
+      def initialize(type, mobile, vcode)
+        self.type = type
+        self.mobile = mobile
+        self.vcode = vcode
+      end
+
+      def call
+        # 检查手机格式是否正确
+        return ApiResult.error_result(MOBILE_FORMAT_WRONG) unless UserValidator.mobile_valid?(mobile)
+
+        #判断验证码是否正确
+        return ApiResult.error_result(VCODE_NOT_MATCH) if vcode != mobile[-4, 4]
+
+        #验证码正确
+        ApiResult.success_result
+      end
+    end
+  end
+end
