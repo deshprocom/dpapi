3e64bc307efad662f3b85a34cd46079ba9a65da7
Ricky <rickycxj@gmail.com>
2017-01-06 19:09:30 +0800

    牌手排行模型 & 文件上传优化
    Former-commit-id: 39d7f4b1d2836cf600ce9191d88dd891a83fd3ca [formerly 72895c7c0941b5ff6ff79bb6819c0b1f2bfc8405] [formerly 34b2876cd8b1a37e2119d1259e99f95119330d36 [formerly 95f7bbc25662e13e7b2e696be66cdf88fe7d05ff]]
    Former-commit-id: 5fe635049530452d4bfbb304a3a32f9e18a3ab0f [formerly c85ad534417405802ab986ce2b5042e76dc1bf65]
    Former-commit-id: eadabbe899117d094cce1baca6e3ea474752e203

--- app/controllers/v10/uploaders/avatar_controller.rb
+++ app/controllers/v10/uploaders/avatar_controller.rb
@@ -7,7 +7,7 @@ module V10
       before_action :login_required
 
       def create
-        @current_user.avatar = upload_params[:avatar]
+        @current_user.avatar = get_upload_file(upload_params[:avatar])
         # 检查文件格式
         if @current_user.avatar.blank? || @current_user.avatar.path.blank?
           return render_api_error(FORMAT_WRONG)
@@ -23,6 +23,26 @@ module V10
 
       def upload_params
         params.permit(:avatar)
+      end
+
+      def get_upload_file(target)
+        return target if target
+        string_file = env['rack.input']
+        san_file = CarrierWave::SanitizedFile.new(string_file)
+        san_file.original_filename = "#{@current_user.id}_tmp#{get_ext_name}"
+        san_file
+      end
+
+      def get_ext_name
+        if env['CONTENT_TYPE'].eql?'image/jpeg'
+          '.jpg'
+        elsif env['CONTENT_TYPE'].eql?'image/png'
+          '.png'
+        elsif env['CONTENT_TYPE'].eql?'image/gif'
+          '.gif'
+        else
+          ''
+        end
       end
     end
   end
