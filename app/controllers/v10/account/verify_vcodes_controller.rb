5c15661016b14adfec73dc1c3f716445c71fb13b
Ricky <rickycxj@gmail.com>
2017-01-10 14:17:15 +0800

    增加邮箱验证码
    Former-commit-id: 418ea524062d14ef23a45021b95622051aca5ecf [formerly 6502b17b8f23131332fefb666dd5c99f9f7089f8] [formerly 37094b37b14214775d410601b1fd27378aa32ea6 [formerly 616deeb3448399655c897872e17ebffe7857d1ed]]
    Former-commit-id: 1237fc68695266f480632edbce4058b07f0b2e30 [formerly f3f145003df7ac3b1028afce6603f14ce3bfe9ba]
    Former-commit-id: 7323028133eb6e0070562ca9599ea3fbf5d1df76

--- app/controllers/v10/account/verify_vcodes_controller.rb
+++ app/controllers/v10/account/verify_vcodes_controller.rb
@@ -1,4 +1,5 @@
-VCODE_TYPES = %w(login register reset_pwd).freeze
+OPTION_TYPES = %w(login register reset_pwd).freeze
+VCODE_TYPES = %w(mobile email).freeze
 
 module V10
   module Account
@@ -8,16 +9,27 @@ module V10
 
       def create
         # 判断验证码验证的类型是否符合
-        render_api_error(VCODE_TYPE_WRONG) unless params[:type].in?(VCODE_TYPES)
-        vcode_service = Services::Account::VerifyVcodeService
-        api_result = vcode_service.call(send_params[:mobile], send_params[:vcode])
-        render_api_error(api_result.code, api_result.msg)
+        return render_api_error(VCODE_TYPE_WRONG) unless send_params[:vcode_type].in?(VCODE_TYPES)
+        return render_api_error(UNSUPPORTED_OPTION_TYPE) unless send_params[:option_type].in?(OPTION_TYPES)
+        send("#{send_params[:vcode_type]}_verify_vcode")
       end
 
       private
 
+      def mobile_verify_vcode
+        mobile_verify_service = Services::Account::MobileVerifyVcodeService
+        api_result = mobile_verify_service.call(send_params[:mobile], send_params[:vcode])
+        render_api_error(api_result.code, api_result.msg)
+      end
+
+      def email_verify_vcode
+        email_verify_service = Services::Account::EmailVerifyVcodeService
+        api_result = email_verify_service.call(send_params[:email], send_params[:vcode])
+        render_api_error(api_result.code, api_result.msg)
+      end
+
       def send_params
-        params.permit(:type, :vcode, :mobile)
+        params.permit(:option_type, :vcode_type, :vcode, :mobile, :email)
       end
     end
   end
