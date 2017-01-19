14542a8118b33fa880196f50d94f2b976d4865f2
breeze-yang <breeze@deshpro.com>
2017-01-19 18:13:20 +0800

    修改recent race 测试用例

--- spec/factories/data_integration/races.rb
+++ spec/factories/data_integration/races.rb
@@ -2,14 +2,22 @@ module DataIntegration
   module Races
     def init_recent_races
       9.times { FactoryGirl.create(:race) }
-      FactoryGirl.create(:race, start_time: 3.days.ago, end_time: 3.days.since, status: 1)
-      FactoryGirl.create(:race, start_time: 8.days.ago, end_time: 5.days.ago)
+      FactoryGirl.create(:race,
+                         begin_date: 3.days.ago.strftime('%Y-%m-%d'),
+                         end_date: 3.days.since.strftime('%Y-%m-%d'),
+                         status: 1)
+      FactoryGirl.create(:race,
+                         begin_date: 8.days.ago.strftime('%Y-%m-%d'),
+                         end_date: 5.days.ago.strftime('%Y-%m-%d'))
       FactoryGirl.create(:race, status: 2)
       FactoryGirl.create(:race, status: 3)
     end
 
     def init_followed_or_ordered_races(user)
-      first_race  = FactoryGirl.create(:race, start_time: 3.days.ago, end_time: 3.days.since, status: 1)
+      first_race  = FactoryGirl.create(:race,
+                                       begin_date: 3.days.ago.strftime('%Y-%m-%d'),
+                                       end_date: 3.days.since.strftime('%Y-%m-%d'),
+                                       status: 1)
       second_race = FactoryGirl.create(:race)
       FactoryGirl.create(:race_follow, race_id: first_race.id, user_id: user.id)
       FactoryGirl.create(:race_order, race_id: second_race.id, user_id: user.id)
