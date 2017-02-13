5acbcea6b3803492afb2db61b42bdd9a45f1d27b
breeze-yang <breeze@deshpro.com>
2017-02-13 17:29:23 +0800

    增加ac_us017的factory api

---
+++ spec/factories/ac_factory/ac_us017.rb
@@ -0,0 +1,20 @@
+module AcFactory
+  class AcUs018 < AcBase
+    def ac_us017_001
+      ticket_params = params.permit(:status,
+                                    :price,
+                                    :name).as_json
+      FactoryGirl.create(:race_ticket, ticket_params)
+    end
+
+    def ac_us017_04
+      race = generate_race
+      user = User.by_email(params[:email])
+      FactoryGirl.create(:race_order, race_id: race.id, user_id: user.id)
+    end
+
+    def ac_us017
+      generate_race
+    end
+  end
+end
