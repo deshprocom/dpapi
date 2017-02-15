50a0551eea7d455c35da88177e736f028941ed31
breeze-yang <breeze@deshpro.com>
2017-02-15 11:51:15 +0800

    修改赛事详情的测试数据

--- spec/factories/ac_factory/ac_base.rb
+++ spec/factories/ac_factory/ac_base.rb
@@ -43,6 +43,7 @@ module AcFactory
 
     def generate_race
       race = FactoryGirl.create(:race, permit_race_parms)
+      FactoryGirl.create(:race_desc, race: race)
       FactoryGirl.create(:race_ticket, race: race)
       race
     end
--- spec/factories/ac_factory/ac_us017.rb
+++ spec/factories/ac_factory/ac_us017.rb
@@ -1,16 +1,10 @@
 module AcFactory
   class AcUs017 < AcBase
-    def ac_us017_001
+    def ac_us017_01
       ticket_params = params.permit(:status,
                                     :price,
                                     :name).as_json
       FactoryGirl.create(:race_ticket, ticket_params)
-    end
-
-    def ac_us017_04
-      race = generate_race
-      user = User.by_email(params[:email])
-      FactoryGirl.create(:race_order, race_id: race.id, user_id: user.id)
     end
 
     def ac_us017
