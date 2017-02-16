fffe50752f8c4770ba2a6c99e882153646219b02
Ricky <rickycxj@gmail.com>
2017-02-16 15:06:35 +0800

    增加获取售票状态的rpsec

--- spec/requests/v10/race/ticket_status_spec.rb
+++ spec/requests/v10/race/ticket_status_spec.rb
@@ -14,11 +14,21 @@ RSpec.describe '/v10/races/:race_id/ticket_status', :type => :request do
 
   context '赛事不存在' do
     it '应当返回code 1100006' do
-      get v10_race_ticket_status_url,
+      get v10_race_ticket_status_url('nonexistent'),
           headers: http_headers
       expect(response).to have_http_status(200)
       json = JSON.parse(response.body)
       expect(json['code']).to eq(1100006)
+    end
+  end
+
+  context '赛事存在' do
+    it '应当返回code 0' do
+      get v10_race_ticket_status_url(race.id),
+          headers: http_headers
+      expect(response).to have_http_status(0)
+      json = JSON.parse(response.body)
+      expect(json['data']['ticket_status']).to be_truthy
     end
   end
 end
