a6be52f86dcb8ac95fa64cd37de5c111f825e3bf
Ricky <rickycxj@gmail.com>
2017-02-23 10:03:01 +0800

    修改migration

--- db/migrate/20170216043637_create_purchase_order.rb
+++ db/migrate/20170216043637_create_purchase_order.rb
@@ -12,7 +12,7 @@ class CreatePurchaseOrder < ActiveRecord::Migration[5.0]
       t.string :mobile, limit: 50, comment: '联系方式'
       t.string :status, limit: 30,
                default: 'unpaid',
-               comment: '订单状态 unpaid-未付款, paid-已付款, canceled-取消'
+               comment: '订单状态 unpaid-未付款, paid-已付款, unshipped-待发货, completed-已完成, canceled-已取消'
       t.timestamps
     end
   end
