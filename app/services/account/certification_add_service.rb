5f3a8c812067e4f7ba7b80e8495635103c77ac62
Ricky <rickycxj@gmail.com>
2017-02-27 11:08:34 +0800

    rubocop

--- app/services/account/certification_add_service.rb
+++ app/services/account/certification_add_service.rb
@@ -1,5 +1,6 @@
 module Services
   module Account
+    # rubocop:disable Metrics/LineLength: 130
     class CertificationAddService
       ID_REGEX = /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/
       include Serviceable
