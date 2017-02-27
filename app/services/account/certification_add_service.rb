c5d07e52bbd6571d15d56f4223f3bb28e3807efc
Ricky <rickycxj@gmail.com>
2017-02-27 11:07:19 +0800

    修改身份验证规则

--- app/services/account/certification_add_service.rb
+++ app/services/account/certification_add_service.rb
@@ -1,6 +1,7 @@
 module Services
   module Account
     class CertificationAddService
+      ID_REGEX = /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/
       include Serviceable
       include Constants::Error::Common
       include Constants::Error::Account
@@ -26,7 +27,7 @@ module Services
         end
 
         # 身份证格式校验
-        unless user_params[:cert_no] =~ /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{4}$/
+        unless user_params[:cert_no] =~ ID_REGEX
           return ApiResult.error_result(CERT_NO_FORMAT_WRONG)
         end
 
