b93e48f3d9f9861d2487c11e2bae70999f7d850d
breeze-yang <breeze@deshpro.com>
2017-03-25 17:54:58 +0800

    给lock_version添加默认0

--- db/migrate/20170324092837_add_lock_version_to_ticket_info.rb
+++ db/migrate/20170324092837_add_lock_version_to_ticket_info.rb
@@ -1,5 +1,5 @@
 class AddLockVersionToTicketInfo < ActiveRecord::Migration[5.0]
   def change
-    add_column :ticket_infos, :lock_version, :integer
+    add_column :ticket_infos, :lock_version, :integer, default: 0
   end
 end
