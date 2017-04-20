f97d4b527d51402123532dfbcb42f6aa9b840006
Ricky <rickycxj@gmail.com>
2017-04-20 17:01:00 +0800

    通过rubocop检查

--- app/controllers/v10/news/types_controller.rb
+++ app/controllers/v10/news/types_controller.rb
@@ -10,16 +10,15 @@ module V10
       end
 
       def show
-
         page_size = permit_params[:page_size].blank? ? '10' : permit_params[:page_size]
         next_id = permit_params[:next_id].blank? ? '0' : permit_params[:next_id]
-        news = InfoType.find(params[:id]).infos.where("id > ?", next_id).limit(page_size)
+        news = InfoType.find(params[:id]).infos.where('id > ?', next_id).limit(page_size)
         next_id = news.last.try(:id) || 0
 
         template = 'v10/news/show'
         render template, locals: { api_result: ApiResult.success_result,
                                    news: news,
-                                   next_id: next_id}
+                                   next_id: next_id }
       end
 
       private
