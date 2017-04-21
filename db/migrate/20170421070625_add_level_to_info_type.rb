a18db89ea895dffc246a3df0569e5233713c9afe
Ricky <rickycxj@gmail.com>
2017-04-21 15:12:30 +0800

    修改migration

--- db/migrate/20170421070625_add_level_to_info_type.rb
+++ db/migrate/20170421070625_add_level_to_info_type.rb
@@ -1,5 +1,5 @@
 class AddLevelToInfoType < ActiveRecord::Migration[5.0]
   def change
-    add_column :info_types, :level, :integer, comment: '类别排序级别'
+    add_column :info_types, :level, :integer, default: 0, comment: '类别排序级别'
   end
 end
