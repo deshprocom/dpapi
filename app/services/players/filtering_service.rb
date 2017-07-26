b071b0197380abcf37b580011b6720c688c17f79
breeze-yang <breeze@deshpro.com>
2017-07-26 10:59:35 +0800

    修改group条件

--- app/services/players/filtering_service.rb
+++ app/services/players/filtering_service.rb
@@ -21,7 +21,7 @@ module Services
           # .where('country like ?', '%中国')
           join_where = "INNER JOIN races ON races.begin_date >= '#{year_first_day}'
 AND races.begin_date <= '#{year_last_day}' AND races.id = race_ranks.race_id"
-          players = Player.joins(join_where).joins(:race_ranks).group(:player_id)
+          players = Player.joins(join_where).joins(:race_ranks).group(:id)
                           .select('players.*', 'SUM(earning) AS dpi_total_earning')
                           .earn_order
                           .offset(@offset).limit(@page_size)
