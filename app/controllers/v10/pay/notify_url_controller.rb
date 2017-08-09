9ddadf0f3ecf3adfcff9497226af14b5c8a3b0c3
Ricky <rickycxj@gmail.com>
2017-08-09 20:04:24 +0800

    添加回掉

--- app/controllers/v10/pay/notify_url_controller.rb
+++ app/controllers/v10/pay/notify_url_controller.rb
@@ -1,15 +1,23 @@
 module V10
   module Pay
     class NotifyUrlController < ApplicationController
-      def index
-        Rails.logger.info 'test'
-        Rails.logger.info permit_params[:Version]
-        Rails.logger.info permit_params[:MerchantId]
-
-        render plain: 'test'
+      def create
+        bill = Bill.new(create_params(permit_params))
+        bill.save!
       end
 
       private
+
+      def create_params(resource)
+        { merchant_id: resource[:MerchantId],
+          order_number: resource[:MerchOrderId],
+          amount: resource[:Amount],
+          pay_time: resource[:PayTime],
+          trade_number: resource[:OrderId],
+          trade_code: resource[:Status],
+          trade_status: 'paid',
+          trade_msg: '交易成功' }
+      end
 
       def permit_params
         params.permit(:Version,
