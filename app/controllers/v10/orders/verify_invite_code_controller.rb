412ab2338193f884e56a81f05a4883c2babfc8a5
Ricky <rickycxj@gmail.com>
2017-09-05 17:24:40 +0800

    同步model

--- app/controllers/v10/orders/verify_invite_code_controller.rb
+++ app/controllers/v10/orders/verify_invite_code_controller.rb
@@ -5,7 +5,7 @@ module V10
       include Constants::Error::Order
       before_action :login_required, :user_self_required
       def create
-        invite_code = params[:invite_code].try(:upcase)
+        invite_code = params[:invite_code]&.upcase
         # 不存在这样的邀请码
         return render_api_error(INVITE_CODE_NOT_EXIST) unless InviteCode.exists?(code: invite_code)
         render_api_success
