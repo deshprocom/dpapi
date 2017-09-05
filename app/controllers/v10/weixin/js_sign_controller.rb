66de62424b2cf3b888e8dda13ac8322f90c29464
Ricky <rickycxj@gmail.com>
2017-09-05 11:23:25 +0800

    公众号签名

--- app/controllers/v10/weixin/js_sign_controller.rb
+++ app/controllers/v10/weixin/js_sign_controller.rb
@@ -2,15 +2,18 @@ module V10
   module Weixin
     class JsSignController < ApplicationController
       before_action :set_wx_authorize
+      include Constants::Error::Auth
+
       def create
-        sign_package = @wx_authorize.get_jssign_package(params[:url])
-        render plain: sign_package.inspect
+        return render_api_error(AUTH_ERROR) unless @wx_authorize.is_valid?
+        @sign_package = @wx_authorize.get_jssign_package(params[:url])
+        render 'v10/weixin/new'
       end
 
       private
 
       def set_wx_authorize
-        @wx_authorize = WeixinAuthorize::Client.new(ENV['APP_ID'], ENV['APP_SECRET'])
+        @wx_authorize = WeixinAuthorize::Client.new(ENV['G_APP_ID'], ENV['G_APP_SECRET'])
       end
     end
   end
