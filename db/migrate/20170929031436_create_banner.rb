1460a3720e38bfc6b9ba4397b5bd3be6ddbcfecf
breeze-yang <breeze@deshpro.com>
2017-09-30 18:06:22 +0800

    增加banner的migration

---
+++ db/migrate/20170929031436_create_banner.rb
@@ -0,0 +1,10 @@
+class CreateBanner < ActiveRecord::Migration[5.0]
+  def change
+    create_table :banners do |t|
+      t.string :image
+      t.references :source, polymorphic: true
+      t.string :link, comment: '链接地址'
+      t.bigint :position, limit: 20, comment: '用于拖拽排序'
+    end
+  end
+end
