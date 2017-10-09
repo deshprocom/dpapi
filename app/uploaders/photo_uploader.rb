f05de1e91830e6c4c33ca7d81bdacc4f8b96ab28
breeze-yang <breeze@deshpro.com>
2017-10-09 11:07:03 +0800

    同步uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,7 +1,7 @@
 class PhotoUploader < BaseUploader
-  # include CarrierWave::ImageOptim
+  include CarrierWave::ImageOptim
 
-  process resize_to_limit: [900, nil]
+  process resize_to_limit: [1080, nil]
   # process :optimize
 
   def filename
