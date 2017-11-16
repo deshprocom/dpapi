c558ef331bcc2c975341a345ef36ac2afdbfb64d
breeze-yang <breeze@deshpro.com>
2017-11-16 18:21:06 +0800

    修改migration

--- db/migrate/20171116061438_optimize_product_order.rb
+++ db/migrate/20171116061438_optimize_product_order.rb
@@ -3,11 +3,6 @@ class OptimizeProductOrder < ActiveRecord::Migration[5.0]
     add_column :product_orders, :freight_free, :boolean,
                default: false, comment: '是否免运费'
 
-    add_column :product_order_items, :refunded, :boolean,
-               default: false, comment: '是否已退款'
-
-    remove_column(:product_order_items, :shipping_status)
-
     add_index :product_orders, :order_number
   end
 end
