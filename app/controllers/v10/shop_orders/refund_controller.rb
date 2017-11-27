5d9bfb019fc21b7cf2131604b44dac863b8c6209
Ricky <rickycxj@gmail.com>
2017-11-27 17:11:26 +0800

    退款的接口

--- app/controllers/v10/shop_orders/refund_controller.rb
+++ app/controllers/v10/shop_orders/refund_controller.rb
@@ -5,13 +5,18 @@ module V10
         order_item = ProductOrderItem.find(params[:item_id])
         refund_type = ProductRefundType.find(params[:product_refund_type_id])
         result = Services::ShopOrders::CreateRefundService.call(user_params, order_item, refund_type)
-        render plain: result.inspect
+        return render_api_error(result.code, result.msg) if result.failure?
+        render plain:result.inspect
+        # render 'v10/shop_order/product_orders/create', locals: { order: result.data[:order] }
       end
 
       private
 
       def user_params
-        params.permit(:product_refund_type_id, :refund_price, :memo, :refund_images)
+        params.permit(:product_refund_type_id,
+                      :refund_price,
+                      :memo,
+                      refund_images: [:id, :content])
       end
     end
   end
