21f6d20f89ae2e71b0ae8d431717dc236e713dc5
Ricky <rickycxj@gmail.com>
2017-12-18 17:25:47 +0800

    增加回复下回复的接口

--- app/controllers/v10/topic/replies_controller.rb
+++ app/controllers/v10/topic/replies_controller.rb
@@ -16,7 +16,7 @@ module V10
       def create
         result = Services::UserAuthCheck.call(@current_user)
         return render_api_error(result.code, result.msg) if result.failure?
-        result = Services::Replies::CreateReplyService.call(user_params, @current_user, @comment)
+        result = params[:reply_id].present? ? parent_reply : parent_comment
         return render_api_error(result.code, result.msg) if result.failure?
         render 'create', locals: { reply: result.data[:reply] }
       end
@@ -33,11 +33,20 @@ module V10
       end
 
       def set_reply
-        @reply = @comment.replies.find_by!(id: params[:id])
+        reply_id = params[:id] || params[:reply_id]
+        @reply = @comment.replies.find_by!(id: reply_id)
       end
 
       def user_params
         params.permit(:body)
+      end
+
+      def parent_comment
+        Services::Replies::CreateReplyService.call(user_params, @current_user, @comment)
+      end
+
+      def parent_reply
+        Services::Replies::CreateReplyService.call(user_params, @current_user, @comment, set_reply)
       end
     end
   end
