4070e70b63827ecb4fdd759c46ee9aa30275c570
Ricky <rickycxj@gmail.com>
2018-04-13 14:21:43 +0800

    修复rubocop

--- app/services/jmessage/create_user.rb
+++ app/services/jmessage/create_user.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/MethodLength
 module Services
   module Jmessage
     class CreateUser
@@ -22,9 +24,7 @@ module Services
         if @user.avatar_path.present?
           close_buffer_limit
           img_result = ::Jmessage::User.upload_image(open(@user.avatar_path))
-          if img_result['media_id'].present?
-            params.merge!(avatar: img_result['media_id'])
-          end
+          params[:avatar] = img_result['media_id'] if img_result['media_id'].present?
           open_buffer_limit
         end
 
@@ -36,7 +36,7 @@ module Services
           return ApiResult.error_result(result.first['error']['code'], result.first['error']['message']) if user.nil?
         end
         # 在数据库创建该用户信息
-        j_user = JUser.create(user_id: @user.id, username: params[:username], password: params[:password] )
+        j_user = JUser.create(user_id: @user.id, username: params[:username], password: params[:password])
         ApiResult.success_with_data(j_user: j_user)
       end
 
@@ -60,15 +60,9 @@ module Services
       def payload
         { username: @user.user_name,
           nickname: @user.nick_name,
-          password: ::Digest::MD5.hexdigest(SecureRandom.uuid)
-        }
+          password: ::Digest::MD5.hexdigest(SecureRandom.uuid) }
       end
 
-      # def avatar_url_to_tempfile(avatar_path)
-      #   stream = open(avatar_path)
-      #   Services::StreamIo.write_stream_to_file(stream) unless stream.is_a? Tempfile
-      # end
-      #
       def close_buffer_limit
         OpenURI::Buffer.send :remove_const, 'StringMax' if string_max_defined?
         OpenURI::Buffer.const_set 'StringMax', 0
