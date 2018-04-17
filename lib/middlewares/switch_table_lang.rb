2b87a8248ad0c1d66658e049e0006660b318b823
breeze-yang <breeze@deshpro.com>
2018-04-17 15:22:58 +0800

    默认使用英文版本

--- lib/middlewares/switch_table_lang.rb
+++ lib/middlewares/switch_table_lang.rb
@@ -9,8 +9,8 @@ module DpAPI
     end
 
     def call(env)
-      lang = (env['HTTP_X_DP_LANG'] || 'zh').strip
-      return wrong_header_lang unless SUPPORT_HEADER_LANG.include?(lang)
+      lang = env['HTTP_X_DP_LANG'].strip
+      lang = 'en' unless SUPPORT_HEADER_LANG.include?(lang)
       switch_lang lang
       @app.call(env)
     end
@@ -39,16 +39,6 @@ module DpAPI
       SWITCH_MODEL_LIST.collect do |t|
         t.safe_constantize.table_name = "#{t.underscore}_#{lang.pluralize}"
       end
-    end
-
-    def wrong_header_lang
-      status = Constants::Error::Http::HTTP_INVALID_HEADER
-      headers = {
-        'Content-Type' => 'application/json',
-        'x-dp-code' => status,
-        'x_dp_msg' => '不支持的请求头.'
-      }
-      [status, headers, []]
     end
   end
 end
