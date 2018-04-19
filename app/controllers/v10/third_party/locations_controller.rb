1c34f5320ddb90664994a6ac8c1531b30bfa2f8f
shootingfly <790174750@qq.com>
2018-04-19 17:05:59 +0800

    修复longitude的拼写错误

--- app/controllers/v10/third_party/locations_controller.rb
+++ app/controllers/v10/third_party/locations_controller.rb
@@ -3,7 +3,7 @@ module V10
     class LocationsController < ApplicationController
       def index
         options = params.slice(:keyword, :page, :pagetoken, :geo_type, :radius, :type)
-        @locations = ::Geo::Location.nearby(params[:latitude], params[:longtitude], options)
+        @locations = ::Geo::Location.nearby(params[:latitude], params[:longitude], options)
         render 'index'
       end
     end
