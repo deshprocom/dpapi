d8739fae6fd2644b5964a5df7e90a72201b71551
shootingfly <790174750@qq.com>
2018-04-23 18:19:07 +0800

    判断用户是否点赞

--- app/controllers/v10/users/topics_controller.rb
+++ app/controllers/v10/users/topics_controller.rb
@@ -1,12 +1,15 @@
 module V10
   module Users
     class TopicsController < ApplicationController
+      include CheckTopicLike
+      before_action :set_user_like_ids
+
       def index
-        @topics = UserTopic.undeleted.sorted.page(params[:page]).per(params[:page_size])
+        @topics = UserTopic.sorted.page(params[:page]).per(params[:page_size]).includes(:topic_images, :counter)
       end
 
       def recommends
-        @topics = UserTopic.undeleted.recommended.sorted.page(params[:page]).per(params[:page_size])
+        @topics = UserTopic.recommended.sorted.page(params[:page]).per(params[:page_size]).includes(:topic_images, :counter)
       end
 
       def details
